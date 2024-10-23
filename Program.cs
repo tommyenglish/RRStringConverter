b0a317bc448cc603127b20eff8f6164831b832b7
tommyenglish <dynadyn@gmail.com>
2024-10-22 22:34:33 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+RR Code Challenge
+Convert the following string: 
+
+`"(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)"`
+
+To this output: 
+
+```
+- id
+- name
+- email
+- type
+  - id
+  - name
+  - customFields
+    - c1
+    - c2
+    - c3
+- externalId
+```
+
+And also to this output:
+
+```
+- email
+- externalId
+- id
+- name
+- type
+  - customFields
+    - c1
+    - c2
+    - c3
+  - id
+  - name
+```
+*/
+Console.WriteLine("Let's go!");
